4051c1e2a745bb49e1e3b3948a08f5a3578e5e88
Guilherme <guischu09@gmail>
2025-06-08 16:25:00 -0300

    Refactor on client code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,159 +1,68 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
+	"mexemexe/internal/client"
 	"mexemexe/internal/engine"
-	"mexemexe/internal/server"
-	"net/url"
-
-	"github.com/gorilla/websocket"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-type RawGamePlayMessage struct {
-	Play json.RawMessage `json:"play"`
-}
+func SigtermListener(signalChan chan os.Signal, stopChan chan struct{}) {
+	sig := <-signalChan
+	fmt.Printf("Received termination signal (%v), shutting down computer vision service.", sig)
 
-var addr = flag.String("addr", "192.168.15.6:8888", "http service address")
+	select {
+	case <-stopChan:
+	default:
+		close(stopChan)
+	}
+}
 
 func main() {
 
-	flag.Parse()
-	log.SetFlags(0)
+	// Instantiate a client
+	client := client.NewClient("192.168.15.6", "8888")
+	defer client.Close()
 
-	var username string
-	fmt.Println("Lets play mexe-mexe!")
-	fmt.Println("To join a game room, please enter your username:")
-	// username = "guilherme"
-	fmt.Scanf("%s", &username)
+	// Set username
+	client.SetUsername()
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
-	log.Printf("connecting to %s\n", u.String())
-
-	// Stablish websocket connection
-	ws, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer ws.Close()
+	// Establish websocket connection
+	client.SetWebsocketConnection()
 
 	// Send join message - Here should enter authentication
-	joinMessage := server.JoinServerMessage{
-		Username: username,
-	}
-	err = ws.WriteJSON(joinMessage)
-	if err != nil {
-		log.Printf("error writing to websocket: %v", err)
-		return
-	}
-	// Read join response from server
-	var welcomeMsg server.WelcomeMessage
+	client.SendJoinMessage()
 
-	err = ws.ReadJSON(&welcomeMsg)
-	if err != nil {
-		log.Printf("error: %v", err)
-		return
-	}
-	playerUUID := welcomeMsg.PlayerUUID
-	fmt.Println(welcomeMsg.Message)
+	// Read join response from server
+	client.ReceiveWelcomeMessage()
 
 	// Send start game message to server -- TODO with game options
-	startGameMessage := server.StartGameMessage{
-		Action: "start",
-	}
-	err = ws.WriteJSON(startGameMessage)
-	if err != nil {
-		log.Printf("error writing to websocket: %v", err)
-		return
-	}
+	client.SendStartGameMessage()
 
 	// Read Join game response from server
-	var joinMsg server.JoinedGameRoomMessage
-	err = ws.ReadJSON(&joinMsg)
-	if err != nil {
-		log.Printf("error: %v", err)
-		return
-	}
-	fmt.Println(joinMsg.Message)
+	client.ReceiveJoinedGameRoomMessage()
 
 	// Read Game started message from server
-	var gameStartedMsg server.GameStartedMessage
-	err = ws.ReadJSON(&gameStartedMsg)
-	if err != nil {
-		log.Printf("error: %v", err)
-		return
-	}
-	fmt.Println(gameStartedMsg.Message)
+	client.ReceiveGameStartedMessage()
 
-	renderer := engine.NewRenderer(username)
+	// Set renderer
+	renderer := engine.NewRenderer(client.Username)
+	client.SetRenderer(renderer)
+
+	// Setup signal and stop channels
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	stopChan := make(chan struct{})
+
+	// Start signal listener
+	go SigtermListener(signalChan, stopChan)
+
+	// Start the game
 	stopDisplay := make(chan bool)
 
-	// Game started! Handle the request-response pattern
-	for {
-		fmt.Println("DEBUG: beggining of loop.")
-		// Wait for game state from server
-		var gameState server.GameStateMessage
-		err := ws.ReadJSON(&gameState)
-		if err != nil {
-			log.Printf("error reading game state: %v", err)
-			return
-		}
+	// Start the game!
+	client.StartGame(stopDisplay)
 
-		fmt.Println("DEBUG: Received game state: ")
-		// fmt.Println("DEBUG: Game state: ", gameState)
-		log.Print("player :: !> DEBUG: Game state: gameState.Hand: ")
-		gameState.Hand.Print()
-		log.Print("player :: !> DEBUG: Game state: gameState.Table: ")
-		gameState.Table.Print()
-		log.Print("player :: !> DEBUG: Turn state: turnState.HasDrawedCard: ", gameState.Turn.HasDrawedCard)
-		log.Print("player :: !> DEBUG: Turn state: turnState.HasPlayedMeld: ", gameState.Turn.HasPlayedMeld)
-		log.Print("player :: !> DEBUG: Turn state: turnState.PlayerUUID: ", gameState.Turn.PlayerUUID)
-
-		// Stop any existing display
-		select {
-		case stopDisplay <- true:
-		default:
-		}
-
-		// Determine if it's the player's turn
-		freeze := gameState.Turn.PlayerUUID != playerUUID
-
-		// fmt.Println("This player uuid: ", playerUUID)
-		// fmt.Println("Turn player uuid: ", gameState.Turn.PlayerUUID)
-
-		renderer.UpdateRenderer(gameState.Table, gameState.Hand, gameState.Turn)
-
-		// fmt.Println("Freeze: ", freeze)
-
-		var play engine.Play
-		if freeze {
-			play = renderer.DisplayScreen(stopDisplay)
-
-		} else {
-			play = renderer.UserInputDisplay(stopDisplay)
-		}
-
-		fmt.Println("Play: ", play)
-		fmt.Println("Play type: ", play.GetName())
-		fmt.Println("Play cards: ", play.GetCards())
-
-		// Send the play back to server immediately
-		if play != nil {
-			var gamePlayMsg server.GamePlayMessage
-			gamePlayMsg.Play = play
-
-			err := ws.WriteJSON(&gamePlayMsg)
-			if err != nil {
-				log.Printf("error writing to websocket: %v", err)
-				return
-			}
-
-			// If it's a quit play, break the loop
-			if play.GetName() == engine.QUIT {
-				break
-			}
-		}
-	}
 }
---
+++ internal/client/client.go
@@ -0,0 +1,189 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"mexemexe/internal/engine"
+	"mexemexe/internal/server"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+// Client defines a connected client
+type Client struct {
+	ServerIP   string
+	ServerPort string
+	Renderer   *engine.Rederer
+	Username   string
+	UUID       string
+	Conn       *websocket.Conn
+}
+
+// NewClient is Client constructor
+func NewClient(ip string, port string) *Client {
+	client := Client{
+		ServerIP:   ip,
+		ServerPort: port,
+	}
+	return &client
+}
+
+// SetUsername sets the username for the client from the user input
+func (c *Client) SetUsername() {
+	var username string
+	fmt.Println("Lets play mexe-mexe!")
+	fmt.Println("To join a game room, please enter your username:")
+	fmt.Scanf("%s", &username)
+	c.Username = username
+}
+
+// SetWebsocketConnection establishes a websocket connection to the server
+func (c *Client) SetWebsocketConnection() {
+	url := url.URL{Scheme: "ws", Host: c.ServerIP + ":" + c.ServerPort, Path: "/ws"}
+	log.Printf("connecting to %s\n", url.String())
+
+	// Stablish websocket connection
+	ws, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	c.Conn = ws
+}
+
+func (c *Client) SendJoinMessage() {
+	joinMessage := server.JoinServerMessage{
+		Username: c.Username,
+	}
+	err := c.Conn.WriteJSON(joinMessage)
+	if err != nil {
+		log.Printf("error writing to websocket: %v", err)
+		return
+	}
+}
+
+func (c *Client) ReceiveWelcomeMessage() {
+	var welcomeMsg server.WelcomeMessage
+
+	err := c.Conn.ReadJSON(&welcomeMsg)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+	c.UUID = welcomeMsg.PlayerUUID
+	fmt.Println(welcomeMsg.Message)
+}
+
+func (c *Client) SendStartGameMessage() {
+	startGameMessage := server.StartGameMessage{
+		Action: "start",
+	}
+	err := c.Conn.WriteJSON(startGameMessage)
+	if err != nil {
+		log.Printf("error writing to websocket: %v", err)
+		return
+	}
+}
+
+func (c *Client) ReceiveJoinedGameRoomMessage() {
+	var joinMsg server.JoinedGameRoomMessage
+	err := c.Conn.ReadJSON(&joinMsg)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+	fmt.Println(joinMsg.Message)
+}
+
+func (c *Client) ReceiveGameStartedMessage() {
+	var gameStartedMsg server.GameStartedMessage
+	err := c.Conn.ReadJSON(&gameStartedMsg)
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+	fmt.Println(gameStartedMsg.Message)
+}
+
+func (c *Client) SetRenderer(renderer *engine.Rederer) {
+	c.Renderer = renderer
+}
+
+func (c *Client) StartGame(stopDisplay chan bool) {
+
+	for {
+		fmt.Println("DEBUG: beggining of loop.")
+		// Wait for game state from server
+		var gameState server.GameStateMessage
+		err := c.Conn.ReadJSON(&gameState)
+		if err != nil {
+			log.Printf("error reading game state: %v", err)
+			return
+		}
+
+		fmt.Println("DEBUG: Received game state: ")
+		// fmt.Println("DEBUG: Game state: ", gameState)
+		log.Print("player :: !> DEBUG: Game state: gameState.Hand: ")
+		gameState.Hand.Print()
+		log.Print("player :: !> DEBUG: Game state: gameState.Table: ")
+		gameState.Table.Print()
+		log.Print("player :: !> DEBUG: Turn state: turnState.HasDrawedCard: ", gameState.Turn.HasDrawedCard)
+		log.Print("player :: !> DEBUG: Turn state: turnState.HasPlayedMeld: ", gameState.Turn.HasPlayedMeld)
+		log.Print("player :: !> DEBUG: Turn state: turnState.PlayerUUID: ", gameState.Turn.PlayerUUID)
+
+		// Stop any existing display
+		select {
+		case stopDisplay <- true:
+		default:
+		}
+
+		// Determine if it's the player's turn
+		freeze := gameState.Turn.PlayerUUID != c.UUID
+		// fmt.Println("This player uuid: ", clientUUID)
+		// fmt.Println("Turn player uuid: ", gameState.Turn.PlayerUUID)
+
+		c.Renderer.UpdateRenderer(gameState.Table, gameState.Hand, gameState.Turn)
+
+		// fmt.Println("Freeze: ", freeze)
+
+		var play engine.Play
+		if freeze {
+			play = c.Renderer.DisplayScreen(stopDisplay)
+
+		} else {
+			play = c.Renderer.UserInputDisplay(stopDisplay)
+		}
+
+		fmt.Println("Play: ", play)
+		fmt.Println("Play type: ", play.GetName())
+		fmt.Println("Play cards: ", play.GetCards())
+
+		// Send the play back to server immediately
+		if play != nil {
+			var gamePlayMsg server.GamePlayMessage
+			gamePlayMsg.Play = play
+
+			err := c.Conn.WriteJSON(&gamePlayMsg)
+			if err != nil {
+				log.Printf("error writing to websocket: %v", err)
+				return
+			}
+
+			// If it's a quit play, break the loop
+			if play.GetName() == engine.QUIT {
+				break
+			}
+		}
+	}
+
+}
+
+// Close closes the websocket connection
+func (c *Client) Close() {
+	c.Conn.Close()
+}
+
+type RawGamePlayMessage struct {
+	Play json.RawMessage `json:"play"`
+}
